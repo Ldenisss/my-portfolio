46a822f61dc483ff493a32b32fa15812b93a5770
unknown <denbmw921@gmail.com>
2018-03-17 22:05:19 +0300

    'about_page'

---
+++ build/js/about.js
@@ -0,0 +1,40 @@
+webpackJsonp([1],{
+
+/***/ 10:
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 8:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__base_scss__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__base_scss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__base_scss__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__commons_fonts_roboto_scss__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__commons_fonts_roboto_scss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__commons_fonts_roboto_scss__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__commons_fonts_sansus_sansus_scss__ = __webpack_require__(9);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__commons_fonts_sansus_sansus_scss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2__commons_fonts_sansus_sansus_scss__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__about_scss__ = __webpack_require__(10);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__about_scss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3__about_scss__);
+
+
+
+
+
+
+console.log("about")
+
+/***/ }),
+
+/***/ 9:
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+
+},[8]);
